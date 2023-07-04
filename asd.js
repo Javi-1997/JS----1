2e6afe69ead2d52c3a6dcc878ea0cb61ca7e0807
Javier Laufgang <121068091+Javi-1997@users.noreply.github.com>
2023-07-03 21:12:59 -0300

    Add files via upload

---
+++ asd.js
@@ -0,0 +1,22 @@
+function notebook()
+let notebook = prompt("Selecione producto")
+switch(notebook){
+   case"hp": 
+    break;
+
+    case"lenovo": 
+    break;
+
+    case"apple": 
+    break;
+
+    case"bgh": 
+    break;
+
+    case"dell": 
+    break;
+
+    default:
+        alert("no has seleccionado un producto")
+        break;
+}
